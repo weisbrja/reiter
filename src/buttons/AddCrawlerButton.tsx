691c74ef396195009ed71b14ccfed53135924bda
Fabian <girod95@web.de>
2024-10-15 02:04:30 +0200

    Add form for adding crawlers

--- src/buttons/AddCrawlerButton.tsx
+++ src/buttons/AddCrawlerButton.tsx
@@ -1,7 +1,26 @@
+import { useState } from "preact/hooks";
+import AddCrawlerForm from "../forms/AddCrawlerForm";
+import { Popup } from "../components/Popup";
+
 export default function AddCrawlerButton() {
+	const [showForm, setShowForm] = useState(false);
+
+
+	function handleSubmit(crawler: string) {
+		console.log("Crawler added: ", crawler);
+		setShowForm(false);
+	}
+
 	return (
 		<div class="flex items-center justify-center p-4">
-			<button class="btn btn-success">+</button>
+			<button class="btn btn-success" onClick={() => setShowForm(true)}>
+				+
+			</button>
+			{ showForm && (
+				<Popup title="Add Crawler" prevError={""} onCancel={() => setShowForm(false)}>
+					<AddCrawlerForm onSubmit={(crawler) => handleSubmit(crawler)} />
+				</Popup>
+			)}
 		</div>
 	);
 }
