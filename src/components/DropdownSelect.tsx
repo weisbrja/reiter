f1c3e2f429112f2e092ec8680e5f48c78284f03e
weisbrja@x1 <weisbrja@protonmail.com>
2024-10-16 00:27:19 +0200

    improve dropdown, but still not perfect

--- src/components/DropdownSelect.tsx
+++ src/components/DropdownSelect.tsx
@@ -1,52 +1,58 @@
 import { useState } from 'preact/hooks'
 
 export default function DropdownSelect({
-	onSelect,
-	children,
+    onSelect,
+    children,
 }: {
-	onSelect: (selected: string) => void
-	children: string[]
+    onSelect: (selected: string) => void
+    children: string[]
 }) {
-	const [selected, setSelected] = useState<string>()
+    const [selected, setSelected] = useState<string>()
+    const [dropdownOpen, setDropdownOpen] = useState(false)
 
-	function toggleDropdown() {
-		const dropdown = document.getElementById('customDropdown')
-		if (dropdown) {
-			dropdown.classList.toggle('hidden')
-		}
-	}
+    function toggleDropdown() {
+        setDropdownOpen(!dropdownOpen)
+    }
 
-	function handleSelect(selected: string) {
-		setSelected(selected)
-		onSelect(selected)
-		toggleDropdown()
-	}
+    function handleSelect(selected: string) {
+        setSelected(selected)
+        onSelect(selected)
+        toggleDropdown()
+    }
 
-	return (
-		<div class="mb-4">
-			<div
-				class="input input-bordered w-full cursor-pointer flex justify-between items-center"
-				onClick={toggleDropdown}
-			>
-				<span>{selected}</span>
-				<svg
-					class="w-4 h-4 text-primary-content"
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-				>
-					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-				</svg>
-			</div>
-			<div id="customDropdown" class="customDropdown hidden bg-base-100 rounded-box z-10">
-				<ul>
-					{children.map((child) => (
-						<li class="ml-4" onClick={() => handleSelect(child)}>
-							{child}
-						</li>
-					))}
-				</ul>
-			</div>
-		</div>
-	)
+	// TODO: something with tabIndex
+	// FIXME: there's a random empty string at the end of child it seems
+    return (
+        <div class="mb-4">
+            <div class="relative w-full">
+                <div
+                    class="input input-bordered w-full cursor-pointer flex justify-between items-center"
+                    onClick={toggleDropdown}
+                >
+                    <span>{selected}</span>
+                    <svg
+                        class="w-4 h-4 text-primary-content"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                    </svg>
+                </div>
+                {dropdownOpen && (
+                    <ul class="absolute mt-2 w-full rounded-box bg-base-100 z-10">
+                        <div class="mt-2 mb-2">
+                            {children.map((child, index) => (
+                                <li key={index} onClick={() => handleSelect(child)}>
+                                    <div class="hover:bg-base-300 mr-2 ml-2 card">
+                                        <span class="p-2">{child}</span>
+                                    </div>
+                                </li>
+                            ))}
+                        </div>
+                    </ul>
+                )}
+            </div>
+        </div>
+    )
 }
